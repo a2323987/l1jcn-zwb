2305340d5feb169805df2b66e58c01bbc71f877e
zhaowenbing20121013 <zhaowenbing20121013@gmail.com>
2012-10-06 11:55:36 +0000

    融合道具使用 在物品类型哪里设置ItemBlend

--- db/update/etcitem.sql
+++ db/update/etcitem.sql
@@ -1,1 +1,3 @@
 ﻿alter table etcitem add CheckFameLV int(10) NOT NULL DEFAULT '0';
+
+INSERT INTO `etcitem` VALUES ('60016', '武器融合证书', '武器融合证书', '武器融合证书', 'ItemBlend', 'normal', 'glass', '630', '468', '22', '25', '', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', null, '0');
